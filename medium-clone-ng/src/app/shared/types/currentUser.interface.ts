3d6f230904680f955b6d6ba30165b72a059ea61c
massi.brero@gmail.com <massi.brero@gmail.com>
2022-01-17 07:56:14 +0100

    first commit

---
+++ medium-clone-ng/src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,10 @@
+export interface CurrentUserInterface {
+  bio: string,
+  image: string,
+  username: string,
+  email: string,
+  id: string,
+  updatedAt: string,
+  createdAt: string,
+  token: string,
+}
