b56f1908abaed61b149c21c75009b38f62a939d5
massi.brero@gmail.com <massi.brero@gmail.com>
2022-01-22 12:46:08 +0100

    topbar 1

---
+++ medium-clone-ng/src/app/shared/modules/backend-error-messages/backendErrorMessages/components/backend-error-messages/backend-error-messages.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {BackendErrorsInterface} from "../../../../../types/backendErrors.interface";
+
+@Component({
+  selector: 'mc-backend-error-messages',
+  templateUrl: './backend-error-messages.component.html',
+  styleUrls: ['./backend-error-messages.component.scss']
+})
+export class BackendErrorMessagesComponent implements OnInit {
+  @Input() backendErrors: BackendErrorsInterface
+  errorMessages: string[]
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.errorMessages = Object.keys(this.backendErrors).map((name: string) => {
+      const messages = this.backendErrors[name].join(' ')
+
+      return `${name} ${messages}`
+    })
+  }
+}
